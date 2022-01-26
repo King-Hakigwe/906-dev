a162285d12a2e1ba85de7f6952122ab9da19cd8c
King-Hakigwe <davidigwe31@gmail.com>
2022-01-26 07:35:50 +0100

    fixed css

--- src/Containers/Service/service.jsx
+++ src/Containers/Service/service.jsx
@@ -9,7 +9,7 @@ const Service = (props) => {
     let serviceStyle = {backgroundImage: `url(${props.background})  center center/cover`,backgroundColor: 'rgba(40, 92, 136, 0.50)', backgroundBlendMode: 'multiply'};
     
     return (
-          <div className="nine__service" >
+          <div className="nine__service" style={{backgroundImage: `url(${props.background})`}}>
             <div className="nine__service-content">
              <div className="nine__service-content_header">
                  <h2>{props.header}</h2>
